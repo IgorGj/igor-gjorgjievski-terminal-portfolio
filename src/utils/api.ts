353144759b8f091077f13783eb59d16ec41c0eec
Igor Gjorgjievski <igor.gjorgjievski@scito.com>
2024-04-29 14:25:49 +0200

    Added the cv to public

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import config from '../../config.json';
-
+// import fs from 'fs';
+// import path from 'path';
 export const getProjects = async () => {
   const { data } = await axios.get(
     `https://api.github.com/users/${config.social.github}/repos`,
@@ -23,3 +24,18 @@ export const getQuote = async () => {
     quote: `“${data.content}” — ${data.author}`,
   };
 };
+
+// export default function handler(req, res) {
+//   const filePath = path.join(process.cwd(), 'public', 'Igor-Gjorgjievski-CV.pdf');
+
+//   // Check if the file exists
+//   if (!fs.existsSync(filePath)) {
+//     return res.status(404).json({ error: 'File not found' });
+//   }
+
+//   // Stream the file to the client
+//   const fileStream = fs.createReadStream(filePath);
+//   res.setHeader('Content-Type', 'application/pdf');
+//   res.setHeader('Content-Disposition', 'attachment; filename=CV.pdf');
+//   fileStream.pipe(res);
+// }
